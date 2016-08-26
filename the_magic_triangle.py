837c5caef658c02f1f47de9ae4ad7ee160bedfdc
Nils Plath <enpe@posteo.de>
2016-08-26 10:36:21 +0200

    Move card configuration to separate files and remove global variables

--- the_magic_triangle.py
+++ the_magic_triangle.py
@@ -1,67 +1,6 @@
-""" The Magic Triangle
+"""The Magic Triangle"""
 
-                / \    
-               /   \   
-              /0a 0b\  
-             /   0   \ 
-            /    0c   \ 
-            ----------- 
-          / \    1c   / \     
-         /   \   7   /   \    
-        /2a 2b\1b 1a/8b 8a\   
-       /   8   \   /   1   \ 
-      /    2c   \ /    8c   \ 
-      ----------- ----------- 
-    / \    3c   / \    6c   / \     
-   /   \   4   /   \   3   /   \    
-  /4a 4b\3b 3a/5a 5b\6b 6a/7a 7b\   
- /   2   \   /   6   \   /   5   \ 
-/    4c   \ /    5c   \ /    7c   \ 
------------ ----------- ----------- 
-"""
-
-# Deck of cards
-#
-# HO - Head orange
-# HG - Head green
-# HW - Head white
-# BO - Body orange
-# BG - Body green
-# BW - Body white
-#
-# Cf. doc/snoopy-the-magic-triangle.jpg
-deck = [
-	# a     b     c
-	("HO", "BW", "HW"), # 0
-	("HG", "BG", "HW"), # 1
-	("BG", "HG", "HO"), # 2
-	("HO", "BG", "BW"), # 3
-	("BO", "BG", "HO"), # 4
-	("BO", "HO", "BG"), # 5
-	("HG", "BG", "HO"), # 6
-	("BW", "BG", "HW"), # 7
-	("HO", "BW", "BO")] # 8
-
-# Sides of neighboring triangles that need to be checked. These are
-# expressed as the edges of a dual graph.
-#
-# Cf. doc/triangle-checks.png
-edge_matches = [
-	#+--------- card 
-	#|  +------ neighboring card
-	#|  |  +--- shared side (a=0, b=1, c=2)
-	#|  |  |
-	#V  V  V
-	(0, 1, 2),
-	(1, 2, 1),
-	(2, 3, 2),
-	(3, 4, 1),
-	(3, 5, 0),
-	(5, 6, 1),
-	(6, 7, 0),
-	(6, 8, 2),
-	(1, 8, 0)]
-
+import csv
 
 def rotate(card, count):
 	"""Rotate a card CCW (neg. values) or CW (pos. values) in multiples 
@@ -93,32 +32,32 @@ def rotate(card, count):
 	return tuple(rotated)
 
 
-def shuffle_deck(c):
+def shuffle_deck(config, deck):
 	"""Shuffle the deck of cards as specified by the configuration c."""
-	return [rotate(deck[x[0]], x[1]) if x is not None else None for x in c]
+	return [rotate(deck[c[0]], c[1]) if c is not None else None for c in config]
 
 
-def is_complete(c):
+def is_complete(config, deck):
 	"""Returns true if the configuration contains all cards from the deck, otherwise false."""
-	if c is None:
+	if config is None:
 		return False
 	else:
-		return None not in c and len(c) == len(deck)
+		return None not in config and len(config) == len(deck)
 
 
-def is_solution(c, partial=False):
+def is_solution(config, deck, partial=False):
 	"""Returns true if the configuration is a valid solution, otherwise false. A pair of triangles
 	match, if the body parts  printed on the neighboring sides of the triangles are complements 
 	(head and body), i.e. if they are not equal, and if the colors are the same."""
-	if c is None:
-		print "c is None"
+	if config is None:
+		print "config is None"
 		return False
 
-	shuffled = shuffle_deck(c)
+	shuffled_deck = shuffle_deck(config, deck)
 
 	for edge in edge_matches:
-		card0 = shuffled[edge[0]]
-		card1 = shuffled[edge[1]]
+		card0 = shuffled_deck[edge[0]]
+		card1 = shuffled_deck[edge[1]]
 		if None in [card0, card1]:
 			if partial:
 				continue
@@ -137,12 +76,7 @@ def is_solution(c, partial=False):
 	return True
 
 
-def reject(c):
-	"""Return true if configuration isn't complete and edge tests already fail, otherwise false."""
-	return not is_solution(c, partial=True)
-
-
-def output(c):
+def output(config, deck):
 	"""Returns string containing a configuration in a human-readablea form."""
 
 	# Debug
@@ -157,8 +91,8 @@ def output(c):
 	# 	      '5a', '5b', '6b', '6a',
 	# 	      '7a', '7b',  '4',  '5',
 	# 	       '7', '4c', '5c', '7c')
-	s = shuffle_deck(c)
-	d = [card[0] for card in c]
+	s = shuffle_deck(config, deck)
+	d = [card[0] for card in config]
 	labels = (s[0][0], s[0][1], d[0],    s[0][2],
 		      s[1][2], d[1],    s[2][0], s[2][1],
 		      s[1][1], s[1][0], s[8][0], s[8][1],
@@ -190,37 +124,37 @@ def output(c):
 ----------- ----------- ----------- 
 """ % labels
 
-	msg = "config = %s\ndeck   = %s\n%s" % (c, s, triangle)
+	msg = "config = %s\ndeck   = %s\n%s" % (config, s, triangle)
 
 	return msg
 
 
-def first(c):
+def first(config, deck):
 	"""Generate a first extension of the current configuration; find the first empty element in the 
 	configuration (None), which will be filled with the next free card. If no empty elements exist, 
 	the return value will be None"""
 	try:
-		first_empty = (x for x in range(len(c)) if c[x] == None).next()
+		first_empty = (i for i in range(len(config)) if config[i] == None).next()
 	except StopIteration:
 		return None
 
 	# Find the next free card that can be added to the configuration at the location determined 
 	# above.
-	new_c = c[:]
-	used_cards = [card[0] for card in new_c if card != None]
+	new_config = config[:]
+	used_cards = [card[0] for card in new_config if card != None]
 	for i in range(len(deck)):
 		if i not in used_cards:
-			new_c[first_empty] = (i, 0) # Add the i-th card, with no rotation (0)
+			new_config[first_empty] = (i, 0) # Add the i-th card, with no rotation (0)
 			break
 
-	return new_c
+	return new_config
 
 
-def next(c):
+def next(config, deck):
 	"""Returns the next possible configuration given the current configuration; will return None if 
 	no next configuration exists."""
 	try:
-		first_empty = (x for x in range(len(c)) if c[x] == None).next()
+		first_empty = (i for i in range(len(config)) if config[i] == None).next()
 	except StopIteration:
 		first_empty = len(deck)
 
@@ -231,11 +165,11 @@ def next(c):
 
 	# Determine the next configuration. If possible, rotate the current
 	# last card, otherwise choose and select a new card.
-	new_c = c[:]
-	current_card = c[last_non_empty][0]
-	current_rotation = c[last_non_empty][1]
+	new_config = config[:]
+	current_card = config[last_non_empty][0]
+	current_rotation = config[last_non_empty][1]
 	if current_rotation < 2:
-		new_c[last_non_empty] = (current_card, current_rotation + 1)
+		new_config[last_non_empty] = (current_card, current_rotation + 1)
 	else:
 		# No next card exists, if the current card is the last card in
 		# the deck. Abort the search.
@@ -244,33 +178,56 @@ def next(c):
 
 		# Determine the next possible card for testing; ignore all cards
 		# that already have been tested before.
-		used_cards = [card[0] for card in new_c if card != None]
+		used_cards = [card[0] for card in new_config if card != None]
 		for i in range(current_card, len(deck)):
 			if i not in used_cards:
-				new_c[last_non_empty] = (i, 0) # Add the i-th card, with no rotation (0)
+				new_config[last_non_empty] = (i, 0) # Add the i-th card, with no rotation (0)
 				break
 	
 	# If the configuration didn't change until now, a next item doesn't
 	# exist and that will be indicated by returning None.
-	if new_c == c:
+	if new_config == config:
 		return None
 
-	return new_c
+	return new_config
 
-def backtrack(c):
+
+def backtrack(config, deck):
 	"""Text-book backtracking, cf. https://en.wikipedia.org/wiki/Backtracking"""
-	if reject(c):
+	
+	# Abort the search in the current subtree if the configuration isn't complete and edge tests 
+	# already fail, otherwise false.
+	if not is_solution(config, deck, partial=True):
 		return
 
-	if is_solution(c):
-		print output(c)
+	if is_solution(config, deck):
+		print output(config, deck)
 
-	cur = first(c)
+	cur = first(config, deck)
 	while cur != None:
-		backtrack(cur)
-		cur = next(cur)
+		backtrack(cur, deck)
+		cur = next(cur, deck)
+
+
+def read_csv(filename):
+	"""Read data from a CSV file. The return value is a list of lists of strings."""
+	data = []
+	with open(filename) as fd:
+		f = filter(lambda row: row[0]!='#', fd)
+		csv_reader = csv.reader(f, delimiter=',', quotechar='"', skipinitialspace=True)
+		for row in csv_reader:
+			data.append(row)
+
+	return data
 
 
 if __name__ == "__main__":
-	c = [None] * len(deck)
-	backtrack(c)
+	# Read the set of the playing cards of the game and the set of checks required to determine 
+	# valid solutions.
+	deck = read_csv('deck_of_cards.csv')
+	edge_matches = [[int(j) for j in i] for i in read_csv('edge_matches.csv')] # Convert str -> int
+	assert len(deck) > 0 and len(edge_matches) > 0
+
+	# Start the search.
+	config = [None] * len(deck)
+	backtrack(config, deck)
